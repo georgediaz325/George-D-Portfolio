91e0ebc0dc199434c5b14b139c625fd9c5895443
George Diaz <georgediaz325@gmail.com>
2022-12-05 21:16:15 -0500

    Changes and examples completed in javascript file, linked javascript file to main web page. Removed test text from java and css html files

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert("JS is cool");
+
+//console.log(prompt("What country are you from?, country name:"));
+
+//console.log(confirm("Continue learning Js?"));
+
+//console.log("I am learnign JS");
+
+ //let favFood = ("Pizza");
+ //let favColor = ("Red");
+ //let favDrink = ("Lemonade");
+//const birthDay = ("04/10/2000");
+
+ //favFood = ("Pasta");
+
+ //console.log(favDrink);
+//console.log(favFood);
+//console.log(birthDay)
+
