0cc919424637d7447bdf453f4368bacf0d27285c
George Diaz <georgediaz325@gmail.com>
2022-12-19 21:36:11 -0500

    Notes for java and ccss have been added as well as the the beginning of the structure to home page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 //alert("JS is cool");
 
 //console.log(prompt("What country are you from?, country name:"));
@@ -17,3 +18,104 @@
 //console.log(favFood);
 //console.log(birthDay)
 
+/* let favDrink = ("Sprite");
+let favNum = 3;
+
+let myFavs = function() {
+    console.log(favDrink, favNum);
+}
+myFavs();
+
+let person = {
+
+    name: "John",
+    age: 30,
+}
+
+console.log(person);
+
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2);
+
+console.log(num1%num2);
+
+let total = 10;
+total++;
+console.log(total);
+
+
+// 20
+
+let xp = 250;
+xp *= 2;
+
+console.log(xp);
+
+
+let thou = 1e5;
+console.log(thou);
+
+console.log(typeof thou);
+
+
+let num = 7;
+let num2 = 2;
+let title = "george";
+
+
+let add7 = num + 7;
+let multiply = num * num2;
+/*let capitalize = figure out how 
+to capitalize 
+first letter in string
+
+
+
+const PI = 3.14159;
+
+console.log(PI.toFixed(3));
+
+num2 = PI .toPrecision(3);
+console.log(num2);
+
+const lightSpeed = 186000;
+console.log(lightSpeed.toExponential());
+
+
+
+let s = 'Hello';
+let d = "Hello";
+let b = `Hello`;
+
+let speech = "Martin Luther said 'I have a dream.'";
+console.log(speech);
+
+let tip = "We can escape quotes by using \\.";
+console.log(tip);
+
+ let phrase = "I am learning ";
+ let phrase2 = "Javascript";
+ let result = phrase + phrase2;
+
+ console.log(result); 
+ console.log(phrase.concat(phrase2));
+
+
+
+let firstName = "George";
+let course = "JavaScript";
+let channel = "Dev Dreamer";
+
+let info = `${firstName} is learning ${course} with ${channel}.`;
+
+console.log(info);
+*/
+
+
+let x = "I am watching Dev Dreamer.";
+console.log(x.toUpperCase());
+
+console.log(x.slice(14,25));
